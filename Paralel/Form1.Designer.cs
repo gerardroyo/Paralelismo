cd8483565f066ecf0312c32edad33fbc3a43b364
Gerard Royo <gerard.royo@mataro.epiaedu.cat>
2020-02-07 19:46:51 +0100

    Agregar archivos de proyecto.

---
+++ Paralel/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Paralel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnNormal = new System.Windows.Forms.Button();
+            this.listViewUser = new System.Windows.Forms.ListView();
+            this.colDNI = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colNom = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colEmail = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listViewUserParalel = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.timer1 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Label();
+            this.btnParalel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnNormal
+            // 
+            this.btnNormal.BackColor = System.Drawing.SystemColors.Control;
+            this.btnNormal.Location = new System.Drawing.Point(161, 18);
+            this.btnNormal.Name = "btnNormal";
+            this.btnNormal.Size = new System.Drawing.Size(75, 23);
+            this.btnNormal.TabIndex = 0;
+            this.btnNormal.Text = "Normal";
+            this.btnNormal.UseVisualStyleBackColor = false;
+            this.btnNormal.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listViewUser
+            // 
+            this.listViewUser.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colDNI,
+            this.colNom,
+            this.colEmail});
+            this.listViewUser.HideSelection = false;
+            this.listViewUser.Location = new System.Drawing.Point(12, 59);
+            this.listViewUser.Name = "listViewUser";
+            this.listViewUser.Size = new System.Drawing.Size(394, 403);
+            this.listViewUser.TabIndex = 1;
+            this.listViewUser.UseCompatibleStateImageBehavior = false;
+            this.listViewUser.View = System.Windows.Forms.View.Details;
+            // 
+            // colDNI
+            // 
+            this.colDNI.Text = "DNI";
+            this.colDNI.Width = 102;
+            // 
+            // colNom
+            // 
+            this.colNom.Text = "Nom";
+            this.colNom.Width = 138;
+            // 
+            // colEmail
+            // 
+            this.colEmail.Text = "Email";
+            this.colEmail.Width = 131;
+            // 
+            // listViewUserParalel
+            // 
+            this.listViewUserParalel.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listViewUserParalel.HideSelection = false;
+            this.listViewUserParalel.Location = new System.Drawing.Point(421, 58);
+            this.listViewUserParalel.Name = "listViewUserParalel";
+            this.listViewUserParalel.Size = new System.Drawing.Size(394, 403);
+            this.listViewUserParalel.TabIndex = 3;
+            this.listViewUserParalel.UseCompatibleStateImageBehavior = false;
+            this.listViewUserParalel.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "DNI";
+            this.columnHeader1.Width = 102;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Nom";
+            this.columnHeader2.Width = 138;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Email";
+            this.columnHeader3.Width = 131;
+            // 
+            // timer1
+            // 
+            this.timer1.AutoSize = true;
+            this.timer1.Location = new System.Drawing.Point(185, 485);
+            this.timer1.Name = "timer1";
+            this.timer1.Size = new System.Drawing.Size(49, 13);
+            this.timer1.TabIndex = 4;
+            this.timer1.Text = "00:00:00";
+            // 
+            // timer2
+            // 
+            this.timer2.AutoSize = true;
+            this.timer2.Location = new System.Drawing.Point(608, 485);
+            this.timer2.Name = "timer2";
+            this.timer2.Size = new System.Drawing.Size(49, 13);
+            this.timer2.TabIndex = 5;
+            this.timer2.Text = "00:00:00";
+            // 
+            // btnParalel
+            // 
+            this.btnParalel.Location = new System.Drawing.Point(582, 18);
+            this.btnParalel.Name = "btnParalel";
+            this.btnParalel.Size = new System.Drawing.Size(75, 23);
+            this.btnParalel.TabIndex = 2;
+            this.btnParalel.Text = "button1";
+            this.btnParalel.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1095, 532);
+            this.Controls.Add(this.btnParalel);
+            this.Controls.Add(this.timer2);
+            this.Controls.Add(this.timer1);
+            this.Controls.Add(this.listViewUserParalel);
+            this.Controls.Add(this.listViewUser);
+            this.Controls.Add(this.btnNormal);
+            this.Cursor = System.Windows.Forms.Cursors.PanNW;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNormal;
+        private System.Windows.Forms.ListView listViewUser;
+        private System.Windows.Forms.ColumnHeader colDNI;
+        private System.Windows.Forms.ColumnHeader colNom;
+        private System.Windows.Forms.ColumnHeader colEmail;
+        private System.Windows.Forms.ListView listViewUserParalel;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Label timer1;
+        private System.Windows.Forms.Label timer2;
+        private System.Windows.Forms.Button btnParalel;
+    }
+}
+
